eac5d8c9fdad655819b5fdc74f5b187f69b6496d
Yazdun <yazduntube@gmail.com>
2022-04-26 12:14:59 +0430

    commit

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,41 +1,19 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { Container } from "../../components";
-import css from "./styles.module.css";
+import useFetch from 'use-http'
+import { Container } from '../../components'
+import { GET_TRENDING } from '../../services'
+import css from './styles.module.css'
 
 export const Home = () => {
-  const [loading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
+  const { data = {} } = useFetch(GET_TRENDING, [])
 
-  useEffect(() => {
-    const execute = async () => {
-      setLoading(true);
-      try {
-        await axios
-          .get(
-            "https://api.giphy.com/v1/gifs/trending?api_key=HjvvLv19smnnDbehOuisjLulGQsP3ZWf&limit=40&rating=g"
-          )
-          .then((res) => setData(res.data.data))
-          .then(() => setLoading(false));
-      } catch (error) {
-        console.log(error);
-        setLoading(false);
-      }
-    };
-    execute();
-  }, []);
   return (
     <Container>
       <div className={css.container}>
-        {data.map((g, i) => {
-          const { images } = g;
-          return (
-            <figure>
-              <img src={images.original.url} alt="" />
-            </figure>
-          );
+        {data.data?.map((gif, index) => {
+          const { images } = gif
+          return <img key={index} src={images.original.url} alt="" />
         })}
       </div>
     </Container>
-  );
-};
+  )
+}
