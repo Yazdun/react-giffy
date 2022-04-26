94ae688d3ec9de8582e5a6d79e8bb857b551307b
Yazdun <yazduntube@gmail.com>
2022-04-26 23:31:49 +0430

    commit

--- src/pages/AnimatedLayout/index.jsx
+++ src/pages/AnimatedLayout/index.jsx
@@ -5,14 +5,18 @@ export const AnimatedLayout = ({ children }) => {
   return (
     <motion.div
       initial={{
+        x: '-10%',
         opacity: 0,
       }}
       animate={{
+        x: 0,
         opacity: 1,
       }}
       exit={{
+        x: '10%',
         opacity: 0,
       }}
+      transition={{ duration: 0.3 }}
     >
       <Container>{children}</Container>
     </motion.div>
