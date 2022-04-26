aa3f06c64c1ed21859c434b30f917292aeed944f
Yazdun <yazduntube@gmail.com>
2022-04-26 14:36:53 +0430

    commit

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -1,14 +1,26 @@
-import css from "./styles.module.css";
-import { Container } from "..";
-import { FaSave } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import css from './styles.module.css'
+import { Container } from '..'
+import { Link } from 'react-router-dom'
+import { Links } from './links'
 
 export const Navigation = () => {
   return (
     <div className={css.nav}>
       <Container sx={css.items}>
         <h3>Giffy</h3>
+        <ul className={css.links}>
+          {Links.map((link, index) => {
+            const { title, icon, url } = link
+            return (
+              <li key={index}>
+                <Link to={url} title={title}>
+                  {icon}
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
       </Container>
     </div>
-  );
-};
+  )
+}
---
+++ src/components/navigation/links.js
@@ -0,0 +1,26 @@
+import { FaSave } from 'react-icons/fa'
+import { FiSearch, FiGithub } from 'react-icons/fi'
+import { AiFillFire } from 'react-icons/ai'
+
+export const Links = [
+  {
+    title: 'trending',
+    icon: <AiFillFire />,
+    url: '/',
+  },
+  {
+    title: 'explore',
+    icon: <FiSearch />,
+    url: '/explore',
+  },
+  {
+    title: 'saved gifs',
+    icon: <FaSave />,
+    url: '/saved',
+  },
+  {
+    title: 'source code',
+    icon: <FiGithub />,
+    url: '/',
+  },
+]
