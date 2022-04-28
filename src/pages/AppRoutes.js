400ac45b391926520cdf85d21aa6487d0dd3049e
Yazdun <yazduntube@gmail.com>
2022-04-28 19:04:03 +0430

    commit

--- src/pages/AppRoutes.js
+++ src/pages/AppRoutes.js
@@ -1,18 +1,21 @@
 import { Explore, Trending, Home, SharedLayout } from './'
 import { Routes, Route, useLocation } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
+import { ScrollToTop } from '../components'
 
 export const AppRoutes = () => {
   const location = useLocation()
   return (
     <AnimatePresence exitBeforeEnter initial={false}>
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Home />} />
-          <Route path="explore" element={<Explore />} />
-          <Route path="trending" element={<Trending />} />
-        </Route>
-      </Routes>
+      <ScrollToTop>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<Home />} />
+            <Route path="explore" element={<Explore />} />
+            <Route path="trending" element={<Trending />} />
+          </Route>
+        </Routes>
+      </ScrollToTop>
     </AnimatePresence>
   )
 }
